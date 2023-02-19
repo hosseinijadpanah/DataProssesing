775b1f28f31fa8b47c44de82cac4b56ab7ec6aca
lucas <hosseinijadpanahsaravi@gmail.com>
2023-02-18 21:51:44 -0500

    addad notes

--- dataLoader.py
+++ dataLoader.py
@@ -6,6 +6,7 @@ yfin.pdr_override()
 start = datetime.datetime(2012, 1, 1)
 end = datetime.datetime.now()
 
+#read data with yahoo finance
 sp500 = yfin.Ticker("SPY").history(period='5y')
 
 plt.plot(sp500.index, sp500['Close'])
