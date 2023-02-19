23725f403dea36c941db67309ee41f072da5a8c1
lucas <hosseinijadpanahsaravi@gmail.com>
2023-02-18 21:43:36 -0500

    sp500 datareader

---
+++ dataLoader.py
@@ -0,0 +1,15 @@
+import datetime
+import matplotlib.pyplot as plt
+import yfinance as yfin
+yfin.pdr_override()
+
+start = datetime.datetime(2012, 1, 1)
+end = datetime.datetime.now()
+
+sp500 = yfin.Ticker("SPY").history(period='5y')
+
+plt.plot(sp500.index, sp500['Close'])
+plt.title('S&P 500 Index from 2012 till Now')
+plt.xlabel('Year')
+plt.ylabel('Price')
+plt.show()
